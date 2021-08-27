8f09b2d499b917316d19224d8547c23c424bf6b6
erickemp84 <eric.kemp84@gmail.com>
2021-08-27 15:10:17 -0500

    made several visual changes to the site

--- src/Components/Forms.jsx
+++ src/Components/Forms.jsx
@@ -4,24 +4,7 @@ const Forms = () => {
 
     return(
         <div className="footer">
-            <div className="subscribe">
-                <div className="sub-form">
-                    <h1>Subscribe for Updates!</h1>
-                    <label for="email">Email:</label>
-                    <input type="text" />
-                    <button type="submit">Submit</button>
-                </div>
-            </div>
-            <div className="guestbook">
-                <h1>Leave a Comment In Our Guest Book!</h1>
-                <label for="email">Name:</label>
-                <input type="text" />
-                <label for="email">Location:</label>
-                <input type="text" />
-                <label for="email">Comment:</label>
-                <input type="text" />
-                <button type="submit">Submit</button>
-            </div>
+            
         </div>
     )
 }
